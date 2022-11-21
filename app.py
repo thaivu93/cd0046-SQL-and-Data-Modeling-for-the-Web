54ddcfa17da3003c22349346b48a1f13dd15e98b
Victor <thaivu.tcnh2@gmail.com>
2022-11-22 00:56:51 +1100

    add models files

--- app.py
+++ app.py
@@ -39,9 +39,11 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(200))
 
     venue = db.relationship('Venue', backref='venue')
-
 
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
@@ -57,6 +59,9 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(200))
 
     artist = db.relationship('Artist', backref='artist')
 
@@ -69,6 +74,7 @@ class Show(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     venue = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
     artist = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
