f6de35d0a70ca75c05eadf2e94a66d3adc20c792
Victor <thaivu.tcnh2@gmail.com>
2022-12-27 19:48:53 +1100

    fixing model and populates database

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 #----------------------------------------------------------------------------#
 
 class Venue(db.Model):
-    __tablename__ = 'Venue'
+    __tablename__ = 'venue'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -22,13 +22,13 @@ class Venue(db.Model):
     seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(200))
 
-    venue = db.relationship('Venue', backref='venue')
+    v_shows = db.relationship('Show', backref='venues')
 
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 class Artist(db.Model):
-    __tablename__ = 'Artist'
+    __tablename__ = 'artist'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -42,17 +42,17 @@ class Artist(db.Model):
     seeking_venue = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(200))
 
-    artist = db.relationship('Artist', backref='artist')
+    a_shows = db.relationship('Show', backref='artists')
 
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 class Show(db.Model):
-    __tablename__ = 'Show'
+    __tablename__ = 'show'
 
     id = db.Column(db.Integer, primary_key=True)
-    venue = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-    artist = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue = db.Column(db.Integer, db.ForeignKey('venue.id') )
+    artist = db.Column(db.Integer, db.ForeignKey('artist.id'))
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
